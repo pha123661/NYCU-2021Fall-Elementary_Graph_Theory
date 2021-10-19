c3d7924f837e9bf7580d788df56a093bc9a77607
pha123661 <pha123661@gmail.com>
2021-10-19 10:48:28 +0800

    WIP

--- HW2-0711239/HW2-3-0711239.cpp
+++ HW2-0711239/HW2-3-0711239.cpp
@@ -7,19 +7,6 @@ vector<int> colors;
 set<int> ans;
 int n, color_types;
 
-bool check_finish(int mask)
-{
-    set<int> visited;
-    for (int i = 0; i < color_types; i++)
-    {
-        if ((mask >> i) & 1) // i is in mask
-        {
-            visited.insert(colors[i]);
-        }
-    }
-    return ans == visited;
-}
-
 long long int solve(int mask, int pos)
 {
     // solved
@@ -27,8 +14,8 @@ long long int solve(int mask, int pos)
         return dp[mask][pos];
 
     // mask = 1's, return home
-    if (check_finish(mask))
-        return 0;
+    if (mask == (1 << n) - 1)
+        return dist[0][pos];
 
     long long int minD = INT_MAX;
     for (int k = 0; k < n; k++)
@@ -51,9 +38,6 @@ int main()
     cin >> n >> e >> color_types;
 
     for (int i = 0; i < color_types; i++)
-        ans.insert(i + 1);
-
-    for (int i = 0; i < color_types; i++)
     {
         cin >> tmp;
         colors.push_back(tmp);
@@ -71,6 +55,6 @@ int main()
     }
 
     dp = vector<vector<long long int>>(1 << n, vector<long long int>(n, -1));
-    cout << solve(1, 0);
+    cout << solve(1, 0) / 2;
     return 0;
 }
