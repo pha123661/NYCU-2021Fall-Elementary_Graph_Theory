e61a4552ebc7dd2d70204a775e86ef5a14a15307
pha123661 <pha123661@gmail.com>
2021-10-19 11:37:46 +0800

    permutation + product cpp

--- HW2-0711239/HW2-3-0711239.cpp
+++ HW2-0711239/HW2-3-0711239.cpp
@@ -1,31 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> cartesian(vector<vector<int>> &v)
+{
+    vector<vector<int>> ret;
+    auto product = [](long long a, vector<int> &b)
+    { return a * b.size(); };
+    const long long N = accumulate(v.begin(), v.end(), 1LL, product);
+    vector<int> u(v.size());
+    for (long long n = 0; n < N; ++n)
+    {
+        lldiv_t q{n, 0};
+        for (long long i = v.size() - 1; 0 <= i; --i)
+        {
+            q = div(q.quot, v[i].size());
+            u[i] = v[i][q.rem];
+        }
+        ret.push_back(u);
+    }
+    return ret;
+}
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int e, tmp;
-    cin >> n >> e >> color_types;
-
-    for (int i = 0; i < color_types; i++)
+    int n, e, k;
+    cin >> n >> e >> k;
+    vector<vector<int>> colors_cities(k, vector<int>()); // colors_cities[color] = vector of cities
+    vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+    for (int i = 0; i < n; i++)
     {
+        int tmp;
         cin >> tmp;
-        colors.push_back(tmp);
+        tmp -= 1;
+        colors_cities[tmp].push_back(i);
     }
-
-    dist = vector<vector<long long int>>(n, vector<long long int>(n, INT_MAX));
-    int u, v, w;
     for (int i = 0; i < e; i++)
     {
+        int u, v, w;
         cin >> u >> v >> w;
-        u--;
-        v--;
-        dist[u][v] = w;
-        dist[v][u] = w;
+        u -= 1;
+        v -= 1;
+        dist[u][v] = dist[v][u] = w;
     }
-
-    dp = vector<vector<long long int>>(1 << n, vector<long long int>(n, -1));
-    cout << solve(1, 0) / 2;
+    vector<vector<int>> colors_product = cartesian(colors_cities);
+    long long int min_cost = INT_MAX;
+    for (auto v : colors_product)
+    {
+        do
+        {
+            long long int cost = 0;
+            for (int i = 1; i < v.size(); i++)
+                cost += dist[v[i - 1]][v[i]];
+            min_cost = min(cost, min_cost);
+        } while (next_permutation(v.begin(), v.end()));
+    }
+    cout << min_cost << endl;
     return 0;
 }
