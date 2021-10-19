e474f56bf472edf93167e50cf641fd57a717ef6c
pha123661 <pha123661@gmail.com>
2021-10-19 11:52:35 +0800

    HW2-3 60分

--- HW2-0711239/HW2-3-0711239.cpp
+++ HW2-0711239/HW2-3-0711239.cpp
@@ -25,10 +25,10 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, e, k;
+    unsigned int n, e, k;
     cin >> n >> e >> k;
     vector<vector<int>> colors_cities(k, vector<int>()); // colors_cities[color] = vector of cities
-    vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+    vector<vector<unsigned int>> dist(n, vector<unsigned int>(n, 20000000));
     for (int i = 0; i < n; i++)
     {
         int tmp;
@@ -44,10 +44,33 @@ int main()
         v -= 1;
         dist[u][v] = dist[v][u] = w;
     }
-    vector<vector<int>> colors_product = cartesian(colors_cities);
+    // floyd warshall
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+
     long long int min_cost = INT_MAX;
-    for (auto v : colors_product)
+    if (k != n)
     {
+        vector<vector<int>> colors_product = cartesian(colors_cities);
+        for (auto v : colors_product)
+        {
+            do
+            {
+                long long int cost = 0;
+                for (int i = 1; i < v.size(); i++)
+                    cost += dist[v[i - 1]][v[i]];
+                min_cost = min(cost, min_cost);
+            } while (next_permutation(v.begin(), v.end()));
+        }
+    }
+    else
+    {
+        vector<int> v;
+        for (int i = 0; i < n; i++)
+            v.push_back(i);
         do
         {
             long long int cost = 0;
