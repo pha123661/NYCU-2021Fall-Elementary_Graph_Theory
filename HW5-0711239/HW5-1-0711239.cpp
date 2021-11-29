331ba7813928735b4acaf71da026511f43af7cfd
pha123661 <pha123661@gmail.com>
2021-11-29 21:15:07 +0800

    5-1 100

--- HW5-0711239/HW5-1-0711239.cpp
+++ HW5-0711239/HW5-1-0711239.cpp
@@ -2,6 +2,44 @@
 #include <climits>
 using namespace std;
 
+int inf = 100000000;
+
+int floyd(vector<vector<int>> &adj)
+{
+    int n = adj.size();
+    vector<vector<int>> dist;
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> tmp;
+        for (int j = 0; j < n; j++)
+            tmp.push_back(adj[i][j]);
+        dist.push_back(tmp);
+    }
+
+    int ans = inf;
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < k; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                ans = min(ans, dist[i][j] + adj[i][k] + adj[k][j]);
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+    if (ans == inf)
+        return -1;
+    else
+        return ans;
+}
+
 int main()
 {
     cin.tie();
@@ -10,7 +48,7 @@ int main()
     cin >> n >> e >> type;
     if (type == 1)
     {
-        vector<vector<int>> adj(n, vector<int>(n, 100000000));
+        vector<vector<int>> adj(n, vector<int>(n, inf));
         for (int i = 0; i < e; i++)
         {
             int u, v, w;
@@ -34,5 +72,16 @@ int main()
     }
     else
     {
+        vector<vector<int>> adj(n, vector<int>(n, inf));
+        for (int i = 0; i < e; i++)
+        {
+            int u, v, w;
+            cin >> u >> v >> w;
+            u = min(u, n) - 1;
+            v = min(v, n) - 1;
+            adj[u][v] = w;
+            adj[v][u] = w;
+        }
+        cout << floyd(adj) << endl;
     }
 }
